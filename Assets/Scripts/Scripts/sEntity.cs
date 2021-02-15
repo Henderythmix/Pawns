42d9995b5e4b44511324c4bd5f506c8e911bc67d
Sulfide <chamacoselles@gmail.com>
2021-02-15 15:39:04 -0800

    Made Entity Creation Easier
    Added an Entity Script which is intended to be general purpose for any kind of entity (barrels, monsters, etc)
    
    Healthbars! (kinda. its part of the Entity Script, but feel free to change it if needed)

---
+++ Assets/Scripts/Scripts/sEntity.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sEntity : MonoBehaviour
+{
+    public float Health;
+    public float MaxHealth;
+    public float Defence;
+    public float Strength;
+    
+    public bool isAlive;
+    public bool CanMove;
+
+    public GameObject Healthbar;
+
+    void Update() {
+        if (Health <= 0) {
+            isAlive = false;
+        } else {
+            isAlive = true;
+        }
+
+        Healthbar.GetComponent<Transform>().localScale = new Vector3(2f*((float)Health/(float)MaxHealth), 0.3f, 0.3f);
+    }
+
+    public void Drop(GameObject obj, int amount) {
+        for (int i=1; i <= amount; i++) {
+            Instantiate(obj, gameObject.GetComponent<Transform>());
+        }
+    }
+
+    public void Die() {
+        Destroy(gameObject);
+    }
+}
